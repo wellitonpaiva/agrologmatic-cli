d99ad09ed9968f87d4270a21762fd63a3d1b1611
Welliton Paiva <wellitonpaiva@gmail.com>
2019-10-11 21:42:41 -0300

    adding landing page and initial screen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-import AppNavigator from './navigation/AppNavigator';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -22,7 +22,7 @@ export default function App(props) {
     return (
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+        <HomeScreen />
       </View>
     );
   }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,66 +5,51 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
+  FlatList,
   Text,
-  TouchableOpacity,
+  ToastAndroid,
   View,
+  TouchableHighlight,
 } from 'react-native';
 
 import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container}>     
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
+          <Text style={styles.welcomeText}>Bem vindo ao AgroLog-Matic</Text>
+          <Image source={require('../assets/images/robot-plant.png')} style={styles.welcomeImage} />
+          <Text style={styles.text}>O que gostaria de fazer?</Text>
+        </View>
+
+        <View>
+          <Text style={styles.textHeader}>Minhas Propriedades</Text>
+          <FlatList
+            data={[
+              { nome: 'Fazenda1', endereco: 'Rua das Flores, 1 - São Raimundo - DR' },
+              { nome: 'Fazenda2', endereco: 'Rua das Flores, 2 - São Raimundo - DR' },
+              { nome: 'Fazenda3', endereco: 'Rua das Flores, 3 - São Raimundo - DR' },
+              { nome: 'Fazenda4', endereco: 'Rua das Flores, 4 - São Raimundo - DR' },
+              { nome: 'Fazenda5', endereco: 'Rua das Flores, 5 - São Raimundo - DR' },
+              { nome: 'Fazenda6', endereco: 'Rua das Flores, 6 - São Raimundo - DR' },
+              { nome: 'Fazenda7', endereco: 'Rua das Flores, 7 - São Raimundo - DR' },
+              { nome: 'Fazenda8', endereco: 'Rua das Flores, 8 - São Raimundo - DR' },
+            ]}
+            renderItem={({ item }) => (
+              <TouchableHighlight onPress={() => handleMinhasPropriedadesPress(item)}>
+                <View style={styles.listItem} >
+                  <Text>{item.nome}</Text>
+                  <Text>{item.endereco}</Text>
+                </View>
+              </TouchableHighlight>
+            )} 
           />
         </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
   );
 }
@@ -73,53 +58,38 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
+function handleMinhasPropriedadesPress(item) {
+  ToastAndroid.show(item.nome, ToastAndroid.SHORT);
+};
 
 const styles = StyleSheet.create({
+  welcomeText: {
+    fontSize: 20,
+    lineHeight: 25,
+    color: 'rgba(0,0,0,0.4)',
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },  
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  myPropertiesContainer: {
+    flex: 1,
+    backgroundColor: '#aaa',
   },
+  
   contentContainer: {
     paddingTop: 30,
   },
@@ -135,64 +105,10 @@ const styles = StyleSheet.create({
     marginTop: 3,
     marginLeft: -10,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  listItem: {
+    marginBottom: 10,
+    marginLeft: 10,
+    borderBottomColor: '#000',
+    borderStyle: 'solid',
   },
 });
