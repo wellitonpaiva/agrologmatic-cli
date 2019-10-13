4b53b0102717913ec9109c4d8885283ebb0593fe
Welliton Paiva <wellitonpaiva@gmail.com>
2019-10-13 15:55:14 -0300

    adding propper navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,44 +10,58 @@ import {
   TouchableHighlight,
 } from 'react-native';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Text style={styles.welcomeText}>Bem vindo ao AgroLog-Matic</Text>
-          <Image source={require('../assets/images/robot-plant.png')} style={styles.welcomeImage} />
-          <Text style={styles.text}>O que gostaria de fazer?</Text>
-        </View>
+class HomeScreen extends React.Component {
 
-        <View>
-          <Text style={styles.textHeader}>Minhas Propriedades</Text>
-          <FlatList
-            data={[
-              { id: 1, nome: 'Fazenda1', endereco: 'Rua das Flores, 1 - São Raimundo - DR' },
-              { id: 2, nome: 'Fazenda2', endereco: 'Rua das Flores, 2 - São Raimundo - DR' },
-              { id: 3, nome: 'Fazenda3', endereco: 'Rua das Flores, 3 - São Raimundo - DR' },
-              { id: 4, nome: 'Fazenda4', endereco: 'Rua das Flores, 4 - São Raimundo - DR' },
-              { id: 5, nome: 'Fazenda5', endereco: 'Rua das Flores, 5 - São Raimundo - DR' },
-              { id: 6, nome: 'Fazenda6', endereco: 'Rua das Flores, 6 - São Raimundo - DR' },
-              { id: 7, nome: 'Fazenda7', endereco: 'Rua das Flores, 7 - São Raimundo - DR' },
-              { id: 8, nome: 'Fazenda8', endereco: 'Rua das Flores, 8 - São Raimundo - DR' },
-            ]}
-            renderItem={({ item }) => (
-              <TouchableHighlight onPress={() => handlePropriedadePress(item)}>
-                <View style={styles.listItem} >
-                  <Text>{item.nome}</Text>
-                  <Text>{item.endereco}</Text>
-                </View>
-              </TouchableHighlight>
-            )}
-          />
-        </View>
-      </ScrollView>
-    </View>
-  );
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#03A9F4',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Text style={styles.welcomeText}>Bem vindo ao AgroLog-Matic</Text>
+            <Image source={require('../assets/images/robot-plant.png')} style={styles.welcomeImage} />
+            <Text style={styles.text}>O que gostaria de fazer?</Text>
+          </View>
+
+          <View>
+            <Text style={styles.textHeader}>Minhas Propriedades</Text>
+            <FlatList
+              data={[
+                { id: 1, nome: 'Fazenda1', endereco: 'Rua das Flores, 1 - São Raimundo - DR' },
+                { id: 2, nome: 'Fazenda2', endereco: 'Rua das Flores, 2 - São Raimundo - DR' },
+                { id: 3, nome: 'Fazenda3', endereco: 'Rua das Flores, 3 - São Raimundo - DR' },
+                { id: 4, nome: 'Fazenda4', endereco: 'Rua das Flores, 4 - São Raimundo - DR' },
+                { id: 5, nome: 'Fazenda5', endereco: 'Rua das Flores, 5 - São Raimundo - DR' },
+                { id: 6, nome: 'Fazenda6', endereco: 'Rua das Flores, 6 - São Raimundo - DR' },
+                { id: 7, nome: 'Fazenda7', endereco: 'Rua das Flores, 7 - São Raimundo - DR' },
+                { id: 8, nome: 'Fazenda8', endereco: 'Rua das Flores, 8 - São Raimundo - DR' },
+              ]}
+              renderItem={({ item }) => (
+                <TouchableHighlight onPress={() => this.props.navigation.navigate('Propriedade')}>
+                  <View style={styles.listItem} >
+                    <Text>{item.nome}</Text>
+                    <Text>{item.endereco}</Text>
+                  </View>
+                </TouchableHighlight>
+              )}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 function handlePropriedadePress(item) {
@@ -103,3 +117,5 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
   },
 });
+
+export default HomeScreen;
--- screens/PropriedadeScreen.js
+++ screens/PropriedadeScreen.js
@@ -4,17 +4,33 @@ import { ExpoLinksView } from '@expo/samples';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-export default function PropriedadeScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
+class PropriedadeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Propriedade',
+    headerStyle: {
+      backgroundColor: '#03A9F4',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {/**
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+        <ExpoLinksView />
+      </ScrollView>
+    );
+  }
 }
+
+export default PropriedadeScreen;
 
 PropriedadeScreen.navigationOptions = {
   title: 'Links',
